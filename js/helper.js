ca8af37f395d8c627969614a8f74bd8e727ff7a1
Caitlin Sweeney <caitlin.marie.sweeney@gmail.com>
2016-06-04 10:25:32 -0600

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -14,9 +14,11 @@ replace the %data% placeholder text you see in them.
 */
 var formattedName = HTMLheaderName.replace("%data%", "Caitlin Sweeney");
 var role = "Web Developer";
-var formattedRole = HtmlheaderRole.replace("%data%"", role);
+var formattedRole = HtmlheaderRole.replace("%data%", role);
+
 $("header").prepend(formattedName);
 $("header").prepend(formattedRole);
+
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
